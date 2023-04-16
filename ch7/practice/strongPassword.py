46e7c5838f798c46370dca53aea7309bcfa5c176
Lisa Meyer <lisamermey@gmail.com>
2023-04-15 21:16:58 -0400

    validates that password has both uppercase and lowercase chars, add test cases

--- ch7/practice/strongPassword.py
+++ ch7/practice/strongPassword.py
@@ -5,16 +5,18 @@
 import re
 
 
-    # 8 character long
-    # both uppercase and lowercase characters
     # at least one digit
 def is_password_strong(password):
     is_strong = has_enough_chars(password)
 
-    print(is_strong)
+    if is_strong:
+        is_strong = has_upper_and_lower_chars(password)
+
+    print(is_strong, password)
 
 
 def has_enough_chars(password):
+    '''validates that password is at least 8 characters long'''
     strong_pass = re.compile(r'(\w{8})+')
     
     is_strong = strong_pass.search(password)
@@ -22,9 +24,26 @@ def has_enough_chars(password):
     return bool(is_strong)
 
 
+def has_upper_and_lower_chars(password):
+    '''validates that password has both uppercase and lowercase letters'''
+    strong_pass = re.compile(r'[A-Z]+[a-z]+')
+
+    is_strong = strong_pass.findall(password)
+
+    return bool(is_strong)
+
+
+
+
 # TEST CASES
 # passing
+is_password_strong('AbCdEfGh')
+is_password_strong('ABCdefgh')
+is_password_strong('ABCdefghIJKlmnOP')
+is_password_strong('AbCdEfGhIJklmnOpqR')
+# failing
 is_password_strong('abcdefgh')
 is_password_strong('abcdefghijklmnop')
-# failing
 is_password_strong('abcde')
+is_password_strong('abcdefgh')
+is_password_strong('ABCDEFGH')
