f2cfd3b4835b83f3799c258cfee3c0e48c02ae3a
Lisa Meyer <lisamermey@gmail.com>
2023-04-15 20:44:24 -0400

    fix comment formatting

--- ch7/practice/dateDetection.py
+++ ch7/practice/dateDetection.py
@@ -8,11 +8,11 @@ from calendar import isleap
 
 def detect_date(date):
     date_regex = re.compile(r'''(
-        (\d{2})    # day
-        (/)          # separator
-        (\d{2})    # month
-        (/)          # separator
-        (\d{4})      # year
+        (\d{2})     # day
+        (/)         # separator
+        (\d{2})     # month
+        (/)         # separator
+        (\d{4})     # year
     )''', re.VERBOSE)
 
     valid_date = date_regex.findall(date)
