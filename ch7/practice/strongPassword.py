6e9f10bff5fb61f5080a263a5f05cb4afb5ebd1e
Lisa Meyer <lisamermey@gmail.com>
2023-04-15 21:37:40 -0400

    final solution: validates that password has at least one digit, refactor test cases

--- ch7/practice/strongPassword.py
+++ ch7/practice/strongPassword.py
@@ -5,12 +5,14 @@
 import re
 
 
-    # at least one digit
 def is_password_strong(password):
     is_strong = has_enough_chars(password)
 
     if is_strong:
         is_strong = has_upper_and_lower_chars(password)
+
+    if is_strong:
+        is_strong = has_a_number(password)
 
     print(is_strong, password)
 
@@ -28,19 +30,26 @@ def has_upper_and_lower_chars(password):
     '''validates that password has both uppercase and lowercase letters'''
     strong_pass = re.compile(r'[A-Z]+[a-z]+')
 
-    is_strong = strong_pass.findall(password)
+    is_strong = strong_pass.search(password)
 
     return bool(is_strong)
 
 
+def has_a_number(password):
+    '''validates that password has at least one digit'''
+    strong_pass = re.compile(r'\d+')
+
+    is_strong = strong_pass.search(password)
+    
+    return bool(is_strong)
 
 
 # TEST CASES
 # passing
-is_password_strong('AbCdEfGh')
-is_password_strong('ABCdefgh')
-is_password_strong('ABCdefghIJKlmnOP')
-is_password_strong('AbCdEfGhIJklmnOpqR')
+is_password_strong('AbC23dEfGh')
+is_password_strong('1ABCdefgh')
+is_password_strong('ABCdefghIJKlmnOP3')
+is_password_strong('1AbCdEf2GhIJklmnOpqR3')
 # failing
 is_password_strong('abcdefgh')
 is_password_strong('abcdefghijklmnop')
