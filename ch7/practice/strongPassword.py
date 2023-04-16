d23066c05d324b5b123affce1764bf8ef4039066
Lisa Meyer <lisamermey@gmail.com>
2023-04-15 21:03:00 -0400

    validates if password has enough characters, add test cases

--- ch7/practice/strongPassword.py
+++ ch7/practice/strongPassword.py
@@ -5,8 +5,26 @@
 import re
 
 
-def is_password_strong(password):
     # 8 character long
     # both uppercase and lowercase characters
     # at least one digit
-    return
+def is_password_strong(password):
+    is_strong = has_enough_chars(password)
+
+    print(is_strong)
+
+
+def has_enough_chars(password):
+    strong_pass = re.compile(r'(\w{8})+')
+    
+    is_strong = strong_pass.search(password)
+
+    return bool(is_strong)
+
+
+# TEST CASES
+# passing
+is_password_strong('abcdefgh')
+is_password_strong('abcdefghijklmnop')
+# failing
+is_password_strong('abcde')
