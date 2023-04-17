fec73ebdee23e8c8593c72aa625afdc40acc9654
Lisa Meyer <lisamermey@gmail.com>
2023-04-17 17:26:44 -0400

    inital commit

---
+++ ch8/practice/sandwichChef.py
@@ -0,0 +1,13 @@
+#! python3
+# sandwichChef.py - A program that asks users for their sandwich preferences and ensures valid inputs.
+
+
+import pyinputplus as pyip
+
+
+def make_me_a_sandwich():
+    return
+
+
+if __name__ == '__main__':
+    make_me_a_sandwich()
