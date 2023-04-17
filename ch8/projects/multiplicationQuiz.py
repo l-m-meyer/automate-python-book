a255e6b33fa0609871b70a6ec999e8572558a662
Lisa Meyer <lisamermey@gmail.com>
2023-04-17 17:12:16 -0400

    refactor to handle_question and start_quiz

--- ch8/projects/multiplicationQuiz.py
+++ ch8/projects/multiplicationQuiz.py
@@ -6,39 +6,48 @@ import pyinputplus as pyip
 import random, time
 
 
-def generate_question(qnum):
-    # pick two random numbers:
-        num1 = random.randint(0,9)
-        num2 = random.randint(0,9)
-
-        prompt = f'#{qnum}: {num1} x {num2} = '
-        return prompt, num1, num2
-
-
-if __name__ == '__main__':
+def start_quiz():
     numberOfQuestions = 10
     correctAnswers = 0
 
     for questionNumber in range(numberOfQuestions):
-        q = generate_question(questionNumber)
-        prompt = q[0]
-        num1 = q[1]
-        num2 = q[2]
+        prompt, num1, num2 = generate_question(questionNumber)
         
-        try:
-            # right answers are handled by allowRegexes
-            # wrong answers are handled by blockRegexes, with a custom message
-            pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)],
-                                blockRegexes=[('.*', 'Incorrect!')],
-                                timeout=8, limit=3)
-        except pyip.TimeoutException:
-            print('Out of time!')
-        except pyip.RetryLimitException:
-            print('Out of tries!')
-        else:
-            # this block runs if no exceptions were raised in the try block.
-            print('Correct!')
-            correctAnswers += 1
-
+        correctAnswers = handle_question(prompt, num1, num2, correctAnswers)
+        
         time.sleep(1)
-    print('Score: %s / %s' % (correctAnswers, numberOfQuestions))
+    print(f'Score: {correctAnswers} / {numberOfQuestions}')
+
+
+def generate_question(qnum):
+    # pick two random numbers:
+    num1 = random.randint(0,9)
+    num2 = random.randint(0,9)
+    
+    prompt = f'#{qnum}: {num1} x {num2} = '
+    
+    return prompt, num1, num2
+
+
+def handle_question(prompt, n1, n2, correctAnswers):
+    try:
+        # right answers are handled by allowRegexes
+        # wrong answers are handled by blockRegexes, with a custom message
+        pyip.inputStr(prompt, 
+                      allowRegexes=[f'^{(n1 * n2)}$'],
+                      blockRegexes=[('.*', 'Incorrect!')],
+                      timeout=8,
+                      limit=3)
+    except pyip.TimeoutException:
+        print('Out of time!')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
+    else:
+        # this block runs if no exceptions were raised in the try block
+        print('Correct!')
+        correctAnswers += 1
+    return correctAnswers
+
+
+if __name__ == '__main__':
+    start_quiz()
