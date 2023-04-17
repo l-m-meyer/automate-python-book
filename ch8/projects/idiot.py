54bb6c1ddcf2bc9a9befa4919650520837bcd7a5
Lisa Meyer <lisamermey@gmail.com>
2023-04-17 15:57:35 -0400

    solution for English yes/no prompt

--- ch8/projects/idiot.py
+++ ch8/projects/idiot.py
@@ -9,3 +9,13 @@ import pyinputplus as pyip
 # - Ask the user if they'd like to know how to keep an idiot busy for hours
 # - If the user answers no, quit
 # - If the user answers yes, go to Step 1
+
+
+while True:
+    prompt = 'Want to know how to keep an idiot busy for hours?\n'
+    response = pyip.inputYesNo(prompt)
+    
+    if response == 'no':
+        break
+
+print('Thank you. Have a nice day.')
