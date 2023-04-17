6867291f04c49b5dd43de62050a7e94c9a6bf35f
Lisa Meyer <lisamermey@gmail.com>
2023-04-17 16:08:31 -0400

    final solution to handle english and spanish prompts

--- ch8/projects/idiot.py
+++ ch8/projects/idiot.py
@@ -5,17 +5,28 @@
 import pyinputplus as pyip
 
 
-# Does the following:
-# - Ask the user if they'd like to know how to keep an idiot busy for hours
-# - If the user answers no, quit
-# - If the user answers yes, go to Step 1
-
-
-while True:
+def en_idiot():
     prompt = 'Want to know how to keep an idiot busy for hours?\n'
     response = pyip.inputYesNo(prompt)
-    
-    if response == 'no':
-        break
+    return response
 
-print('Thank you. Have a nice day.')
+
+def sp_idiot():
+    prompt = '¿Quieres saber cómo mantener ocupado a un idiota durante horas?\n'
+    response = pyip.inputYesNo(prompt, yesVal='sí', noVal='no')
+    return response
+
+
+if __name__ == '__main__':
+    language_preference = pyip.inputChoice(['English', 'Spanish'])
+
+    while True:
+        if language_preference == 'English':
+            response = en_idiot()
+        else:
+            response = sp_idiot()
+        
+        if response == 'no':
+            break
+
+    print('Thank you. Have a nice day.')
