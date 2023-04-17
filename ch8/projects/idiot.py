af80b018c7a58cca3a8acc96be37431478ce78f6
Lisa Meyer <lisamermey@gmail.com>
2023-04-17 12:14:57 -0400

    File setup

---
+++ ch8/projects/idiot.py
@@ -0,0 +1,11 @@
+#! python3
+# idiot.py - How to keep an idito busy for hours.
+
+
+import pyinputplus as pyip
+
+
+# Does the following:
+# - Ask the user if they'd like to know how to keep an idiot busy for hours
+# - If the user answers no, quit
+# - If the user answers yes, go to Step 1
