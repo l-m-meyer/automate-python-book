15a7093504c8a49ad5de83edd3c975d49c808f41
Lisa Meyer <lisamermey@gmail.com>
2023-04-17 20:12:39 -0400

    refactor to dynamically select an operator

--- ch8/projects/multiplicationQuiz.py
+++ ch8/projects/multiplicationQuiz.py
@@ -11,9 +11,9 @@ def start_quiz():
     correctAnswers = 0
 
     for questionNumber in range(numberOfQuestions):
-        prompt, num1, num2 = generate_question(questionNumber)
+        prompt, answer = generate_question(questionNumber)
         
-        correctAnswers = handle_question(prompt, num1, num2, correctAnswers)
+        correctAnswers = handle_question(prompt, answer, correctAnswers)
         
         time.sleep(1)
     print(f'Score: {correctAnswers} / {numberOfQuestions}')
@@ -23,18 +23,38 @@ def generate_question(qnum):
     # pick two random numbers:
     num1 = random.randint(0,9)
     num2 = random.randint(0,9)
+    op = generate_op()
+    answer = get_answer(num1, num2, op)
+   
+    prompt = f'#{qnum}: {num1} {op} {num2} = '
     
-    prompt = f'#{qnum}: {num1} x {num2} = '
-    
-    return prompt, num1, num2
+    return prompt, answer
 
 
-def handle_question(prompt, n1, n2, correctAnswers):
+def generate_op():
+    ops = ['x', '/', '+', '-']
+    op = random.randint(0,3)
+
+    return ops[op]
+    
+    
+def get_answer(n1, n2, op):
+    if op == 'x':
+        return n1 * n2
+    if op == '/':
+        return n1 / n2
+    if op == '+':
+        return n1 + n2
+    if op == '-':
+        return n1 - n2
+    
+
+def handle_question(prompt, answer, correctAnswers):
     try:
         # right answers are handled by allowRegexes
         # wrong answers are handled by blockRegexes, with a custom message
         pyip.inputStr(prompt, 
-                      allowRegexes=[f'^{(n1 * n2)}$'],
+                      allowRegexes=[f'^{answer}$'],
                       blockRegexes=[('.*', 'Incorrect!')],
                       timeout=8,
                       limit=3)
