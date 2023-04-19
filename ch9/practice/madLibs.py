4f128e0520e188f2b2f8d461bdcb1f0fd7b54439
Lisa Meyer <lisamermey@gmail.com>
2023-04-19 16:37:55 -0400

    initial commit

---
+++ ch9/practice/madLibs.py
@@ -0,0 +1,35 @@
+#! python3
+# madLibs.py - Reads in text files and lets the user add their own text anywhere the word
+# ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file.
+
+
+
+def mad_libs():
+    ml = open('./madlib.txt')
+    content = ml.read()
+    
+
+
+def process_text(text):
+    return
+
+
+def get_adjective():
+    return
+
+
+def get_noun():
+    return
+
+
+def get_verb():
+    return
+
+
+def get_adverb():
+    return
+
+
+
+if __name__ == '__main__':
+    mad_libs()
