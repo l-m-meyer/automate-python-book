923985b32c57de2ef632dec5cb75d2c0d141f23f
Lisa Meyer <lisamermey@gmail.com>
2023-04-20 10:14:59 -0400

    initial commit

---
+++ ch9/practice/regexSearch.py
@@ -0,0 +1,14 @@
+#! python3
+# regexSearch.py - Opens all .txt files in a folder and searches for any line
+# that matches a user-supplied regular expression.
+
+
+import re, shelve
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
