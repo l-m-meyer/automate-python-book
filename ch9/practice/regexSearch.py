23425bd77b78449207973db91793a5cb45174a2c
Lisa Meyer <lisamermey@gmail.com>
2023-04-20 10:46:43 -0400

    retrieves user-supplied regex

--- ch9/practice/regexSearch.py
+++ ch9/practice/regexSearch.py
@@ -3,11 +3,23 @@
 # that matches a user-supplied regular expression.
 
 
+import pyinputplus as pyip
 import re, shelve
 
 
 def main():
-    return
+    # get user regex
+    user_regex = get_user_regex()
+    # get number of files in a folder
+        # open each .txt file in folder
+        # read each line in file
+        # if user regex in file line
+            # print line
+    print(user_regex)
+
+
+def get_user_regex():
+    return pyip.inputRegexStr('Enter regex: ')
 
 
 if __name__ == '__main__':
