09ad27fc2c0f27a0c03e2b86229459883ca06f5e
Lisa Meyer <lisamermey@gmail.com>
2023-04-20 11:21:58 -0400

    gets a valid user filepath

--- ch9/practice/regexSearch.py
+++ ch9/practice/regexSearch.py
@@ -4,22 +4,39 @@
 
 
 import pyinputplus as pyip
-import re, shelve
+from pathlib import Path
+import re, os
 
 
 def main():
-    # get user regex
     user_regex = get_user_regex()
+    user_path = get_user_path()
+    search_files(user_regex, user_path)
     # get number of files in a folder
         # open each .txt file in folder
         # read each line in file
         # if user regex in file line
             # print line
-    print(user_regex)
+    print(user_regex, user_path)
 
 
 def get_user_regex():
     return pyip.inputRegexStr('Enter regex: ')
+
+
+def get_user_path():
+    path = pyip.inputStr('Enter filepath: ')
+    basedir = Path(path)
+
+    if not basedir.is_dir():
+        print('Path invalid.')
+        return get_user_path()
+    
+    return basedir
+
+
+def search_files(pattern, fpath):
+    return
 
 
 if __name__ == '__main__':
