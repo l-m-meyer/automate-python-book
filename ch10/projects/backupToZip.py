814479cce1442bfc3cb1fa8e742528a6d01ed48d
Lisa Meyer <lisamermey@gmail.com>
2023-04-21 16:41:09 -0400

    file setup

---
+++ ch10/projects/backupToZip.py
@@ -0,0 +1,32 @@
+#! python3
+# backupToZip.py - Copies an entire folder and its contents into
+# a ZIP file whose filename increments.
+
+
+import zipfile, os
+
+
+def backupToZip(folder):
+    # back up the entire contents of "folder" into a ZIP file
+
+
+    folder = os.path.abspath(folder)        # make sure folder is absolute
+
+
+    # figure out the filename this code should use based on existing files
+    number = 1
+    while True:
+        zipFilename = f'{os.path.basename(folder)}_{str(number)}.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number += 1
+
+
+    # TODO: create the ZIP file
+
+
+    # TODO: walk the entire folder tree and compress the files in each folder.
+    print('Done.')
+
+
+backupToZip('C:\\delicious')
