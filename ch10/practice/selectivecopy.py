926b5f83e69eec7338021be018e8ca2152b2cd74
Lisa Meyer <lisamermey@gmail.com>
2023-04-21 17:09:30 -0400

    initial commit

---
+++ ch10/practice/selectivecopy.py
@@ -0,0 +1,22 @@
+#! python3
+# selectivecopy.py - Copies all files in a folder tree with a certain file extension.
+
+
+import shutil, os, re
+
+
+def main():
+    return
+
+
+# TODO: get folder to copy from and file extension from user
+
+
+# TODO: create regex pattern to identify file extension to copy
+
+
+# TODO: walk the folder tree and copy matching file extensions to a new folder
+
+
+if __name__ == '__main__':
+    main()
