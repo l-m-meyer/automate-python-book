4edb56022e1eddf22503a039a5c6196c3e9e4604
Lisa Meyer <lisamermey@gmail.com>
2023-04-21 21:48:36 -0400

    final solution

--- ch10/practice/selectivecopy.py
+++ ch10/practice/selectivecopy.py
@@ -6,13 +6,6 @@ import pyinputplus as pyip
 import shutil, os, re
 
 
-def main():
-    folder = get_folder()
-    ext = get_ext()
-    print(f'FOLDER: {folder}\nEXTENSION: {ext}')
-    return
-
-
 # get folder to copy from user
 def get_folder():
     folder = pyip.inputFilepath('Enter a filepath for folder to copy: ', mustExist=True)
@@ -41,10 +34,34 @@ def get_ext():
 def find_ext(filename, ext):
     ext_regex = re.compile(f'{ext}')
     mo = ext_regex.search(filename)
-    return mo.group()
+    return mo
 
 
-# TODO: walk the folder tree and copy matching file extensions to a new folder
+# walk the folder tree and copy matching file extensions to a new folder
+def copy_files(folder, ext):
+    new_folder = os.path.join(folder, f'{ext[1:]}_files')
+    
+    if not os.path.exists(new_folder):
+        print(f'Creating new folder: {new_folder}...')
+        make_folder = os.makedirs(new_folder)
+
+    for foldername, subfolders, filenames in os.walk(folder):
+        if foldername == new_folder:
+            continue
+
+        for filename in filenames:
+            match = find_ext(filename, ext)
+            if not match:
+                continue
+            to_copy = os.path.join(foldername, filename)
+            shutil.copy(to_copy, new_folder)
+    print('Done.')
+
+
+def main():
+    folder = get_folder()
+    ext = get_ext()
+    return copy_files(folder, ext)
 
 
 if __name__ == '__main__':
