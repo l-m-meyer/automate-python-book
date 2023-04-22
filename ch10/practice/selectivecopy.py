da673f3e7d6fdc3d290328dd0f7e8ccb8e190c87
Lisa Meyer <lisamermey@gmail.com>
2023-04-21 20:42:18 -0400

    create regex pattern to find ext, fix validation errors in get_ext and get_folder

--- ch10/practice/selectivecopy.py
+++ ch10/practice/selectivecopy.py
@@ -13,26 +13,35 @@ def main():
     return
 
 
-# TODO: get folder to copy from and file extension from user
+# get folder to copy from user
 def get_folder():
     folder = pyip.inputFilepath('Enter a filepath for folder to copy: ', mustExist=True)
 
-    return folder
+    if not os.path.exists(folder):
+        print('Filepath is invalid.')
+        return get_folder()
+    
+    return os.path.abspath(folder)
 
 
+# get file extension from user
 def get_ext():
     ext = pyip.inputStr('Enter a file extension to target (ex: .pdf): ',
                         allowRegexes=[r'^\.([a-z]{2,3})$'],
                         blockRegexes=[r'\d+$'])
-
+    
     if ext[0] != '.':
         print(f'"{ext}" is invalid. Must start with "."')
         return get_ext()
-
+    
     return ext
 
 
-# TODO: create regex pattern to identify file extension to copy
+# create regex pattern to identify file extension to copy
+def find_ext(filename, ext):
+    ext_regex = re.compile(f'{ext}')
+    mo = ext_regex.search(filename)
+    return mo.group()
 
 
 # TODO: walk the folder tree and copy matching file extensions to a new folder
