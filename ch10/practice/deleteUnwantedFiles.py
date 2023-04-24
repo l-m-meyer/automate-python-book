68ead85e3a3413995b931c00a27471bff7e3a97c
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 16:06:08 -0400

    final solution

--- ch10/practice/deleteUnwantedFiles.py
+++ ch10/practice/deleteUnwantedFiles.py
@@ -18,6 +18,7 @@ def get_folder():
 
 
 def get_large_files(folder):
+    """Returns a list of files over 100MB in size."""
     large_files = []
 
     for foldername, subfolders, filenames in os.walk(folder):
@@ -31,25 +32,36 @@ def get_large_files(folder):
 
 
 def compare_file_size(file):
-    """Gets size of file and returns file name if less than 100MB."""
+    """Gets size of file and returns True if file size is less than 100MB, False otherwise."""
     file_size = get_file_size_in_MB(file)
     
-    if file_size <= 100:
+    if file_size <= 0:
         return False
     return True
 
 
 def get_file_size_in_MB(file):
-    """Gets size of file in bytes and converts to MB."""
+    """Gets size of file in bytes and returns file size in MB."""
     file_size = os.path.getsize(file)
     file_size_MB = file_size/(1024 * 1024)
     return file_size_MB
 
 
+def print_large_files(files):
+    """Prints the absolute path of each file found, or prints that no files were found."""
+    if not files:
+        print('No files larger than 100MB found in folder tree.')
+        return
+    
+    print('Files over 100MB in size:')
+    for file in files:
+        print(f'- {file}\n')
+
+
 def main():
     folder = get_folder()
     large_files = get_large_files(folder) 
-    print(large_files)
+    print_large_files(large_files)
 
 
 if __name__ == "__main__":
