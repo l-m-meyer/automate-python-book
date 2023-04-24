81c2c9039dc926545b765ec662eb7c792615b9f8
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 13:36:29 -0400

    defines functions to compare file sizes

--- ch10/practice/deleteUnwantedFiles.py
+++ ch10/practice/deleteUnwantedFiles.py
@@ -7,6 +7,7 @@ import os
 
 
 def get_folder():
+    """Gets folder from user to process for large, unwanted files."""
     folder = pyip.inputFilepath('Enter a filepath for folder to copy: ', mustExist=True)
 
     if not os.path.exists(folder):
@@ -14,6 +15,22 @@ def get_folder():
         return get_folder()
     
     return os.path.abspath(folder)
+
+
+def compare_file_size(file):
+    """Gets size of file and returns file name if less than 100MB."""
+    file_size = get_file_size_in_MB(file)
+    
+    if file_size <= 100:
+        return
+    return file
+
+
+def get_file_size_in_MB(file):
+    """Gets size of file in bytes and converts to MB."""
+    file_size = os.path.getsize(file)
+    file_size_MB = file_size/(1024 * 1024)
+    return file_size_MB
 
 
 def main():
