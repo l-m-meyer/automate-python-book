a9fe91b06764ec8d9421a9e6cfcb831e1bb2957f
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 13:35:16 -0400

    adds tests to compare file sizes

--- ch10/practice/test_deleteUnwantedFiles.py
+++ ch10/practice/test_deleteUnwantedFiles.py
@@ -1,19 +1,34 @@
-import pytest, sys
+import pytest, os
 from deleteUnwantedFiles import *
 
 
 class TestMain:
-    def test_main(self):
+    def test_calls_main(self):
         assert callable(main)
 
 
-    def test_retrieves_folder(self):
+    def test_calls_get_folder(self):
         assert callable(get_folder)
-
 
 
 class TestGetFolder:
     def test_folder_input(self):
         folder = get_folder()
         assert type(folder) is str
+
+
+    def test_calls_compare_file_size(self):
+        assert callable(compare_file_size)
+
+
+class TestCompareFileSize:
+    def test_compare_file_size(self):
+        small_file = compare_file_size('./deleteUnwantedFiles.py')
+        assert small_file is None
+
+    
+    def test_file_size_less_than_100MB(self):
+        small_file = get_file_size_in_MB('./deleteUnwantedFiles.py')
+        
+        assert not small_file > 100
 
