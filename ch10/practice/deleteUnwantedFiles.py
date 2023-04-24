e52ced8357a46c13dc09ba2152fd352138e44f15
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 10:58:49 -0400

    initial commit

---
+++ ch10/practice/deleteUnwantedFiles.py
@@ -0,0 +1,13 @@
+#! python3
+# deleteUnwantedFiles.py - Find and delete large, unwanted files of more than 100MB.
+
+
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
