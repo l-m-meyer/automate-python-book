19db4df9b9c325fea7c49b21222a20758b60985b
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 22:25:01 -0400

    final solution

--- ch12/projects/searchpypi.py
+++ ch12/projects/searchpypi.py
@@ -6,7 +6,7 @@ import requests, sys, webbrowser, bs4
 
 
 print('Searching...')       # display text while downloading the search result page
-res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))      # 'https://google.com/search?q=' 
 res.raise_for_status()
 
 
@@ -15,3 +15,10 @@ soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
 # open a browser tab for each result
 linkElems = soup.select('.package-snippet')
+
+numOpen = min(5, len(linkElems))
+
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
