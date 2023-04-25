7268011186b01bf7e099c0830085668f58916314
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 21:55:50 -0400

    checks for errors during requests

---
+++ ch12/exercises/checkingForErrors.py
@@ -0,0 +1,12 @@
+#! python3
+# checkingForErrors.py
+
+
+import requests
+
+
+res = requests.get('https://inventwithpython.com/page_that_does_not_exist')
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print(f'There was a problem: {exc}')
