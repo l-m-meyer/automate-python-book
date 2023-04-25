6803cc819ff5d3494a83de0c183714629203e959
Lisa Meyer <lisamermey@gmail.com>
2023-04-25 13:18:02 -0400

    initial commit

---
+++ ch12/practice/cliEmailer.py
@@ -0,0 +1,17 @@
+#! python3
+# cliEmailer.py - Logs in and sends an email from command line arguments.
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pyinputplus as pyip
+import sys
+
+
+def main():
+    return
+
+
+if __name__== '__main__':
+    main()
