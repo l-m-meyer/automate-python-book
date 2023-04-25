c64a952b852b3fb0433dd352d358d4da3fac8609
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 21:43:44 -0400

    handle the clipboard content and launch browser

--- ch12/projects/mapIt.py
+++ ch12/projects/mapIt.py
@@ -3,12 +3,15 @@
 # command line or clipboard.
 
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 
 
 if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
 
 
-# TODO: Get address from clipboard.
+webbrowser.open(f'https://www.google.com/maps/place/{address}')
