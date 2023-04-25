1658632050cca4000db59c676cb26c6ebe51b06e
Lisa Meyer <lisamermey@gmail.com>
2023-04-24 22:32:29 -0400

    downloads web page

--- ch12/projects/downloadXkcd.py
+++ ch12/projects/downloadXkcd.py
@@ -9,8 +9,12 @@ url = 'https://xkcd.com'            # starting url
 os.mkdirs('xkcd', exist_ok=True)    # store comics in ./xkcd
 
 while not url.endswith('#'):
-    # TODO: download the page
+    # download the page
+    print(f'Downloading page {url}...')
+    res = requests.get(url)
+    res.raise_for_status()
 
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     # TODO: find the url of the comic image
 
