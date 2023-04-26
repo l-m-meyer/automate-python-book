7a77d115acbfa0affcec38ff085442180a185e24
Lisa Meyer <lisamermey@gmail.com>
2023-04-26 10:38:34 -0400

    initial commit with TODOs

---
+++ ch12/practice/2048.py
@@ -0,0 +1,37 @@
+#! python3
+# 2048.py
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+# TODO: navigate to game website
+# https://gabrielecirulli.github.io/2048/
+
+
+# TODO: send up direction
+
+
+# TODO: send down direction
+
+
+# TODO: send left direction
+
+
+# TODO: send right direction
+
+
+# TODO: play game
+
+
+# TODO: handle game over
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
