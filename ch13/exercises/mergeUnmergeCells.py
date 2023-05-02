614c88be31c9e7d57ab73847434e7c50e11fc07f
Lisa Meyer <lisamermey@gmail.com>
2023-05-02 13:44:23 -0400

    merges and unmerges cells in an excel workbook

---
+++ ch13/exercises/mergeUnmergeCells.py
@@ -0,0 +1,32 @@
+import openpyxl
+
+
+# create a new workbook
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+
+# merge cells
+sheet.merge_cells('A1:D3')
+sheet['A1'] = 'Twelve cells merged together.'
+
+sheet.merge_cells('C5:D5')
+sheet['C5'] = 'Two merged cells.'
+
+
+# save workbook
+wb.save('merged.xlsx')
+
+
+# get merged notebook
+wb = openpyxl('merged.xlsx')
+sheet = wb.active
+
+
+# unmerge cells
+sheet.unmerge_cells('A1:D3')    # Split up cells
+sheet.unmerge_cells('C5:D5')
+
+
+# save workbook
+wb.save('merged.xlsx')
