0a1ea3bd61ce6bc04dfe6c74952ca2de617261c6
Lisa Meyer <lisamermey@gmail.com>
2023-05-02 15:28:42 -0400

    create stylized headers

--- ch13/practice/multiplicationTable.py
+++ ch13/practice/multiplicationTable.py
@@ -3,6 +3,8 @@
 
 
 import openpyxl, sys
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
 import pyinputplus as pyip
 
 
@@ -21,10 +23,27 @@ except IndexError as e:
     n = pyip.inputInt('Enter a number: ')
 
 
-# TODO: create column and row headers in bold
+# create font object headers
+headerFont = Font(size=12, bold=True)
+# sheet['A1'].font = headerFont
+# sheet['1'].font = headerFont
+
+
+# create column and row headers in bold
+for i in range(2, int(n) + 2):
+    # columns
+    sheet.cell(row=1, column=i).value = i - 1
+    sheet.cell(row=1, column=i).font = headerFont
+
+    # rows
+    sheet.cell(row=i, column=1).value = i - 1
+    sheet.cell(row=i, column=1).font = headerFont
+
+
 
 
 # TODO: add formula to calculate values for each cell
 
 
 # TODO: save workbook
+wb.save('multiplicationTable.xlsx')
