19d6dbb93e7b0dc7ee8286110f763371110fe91f
Lisa Meyer <lisamermey@gmail.com>
2023-05-03 11:45:21 -0400

    final solution

--- ch13/practice/blankRowInserter.py
+++ ch13/practice/blankRowInserter.py
@@ -3,27 +3,25 @@
 
 
 import openpyxl, sys
+from openpyxl.utils import get_column_letter
 
 
 # get arguments from command line
 try:
-    row = sys.argv[1]
-    num_blank_rows = sys.argv[2]
+    starting_row = int(sys.argv[1])
+    num_blank_rows = int(sys.argv[2])
     filename = sys.argv[3]
 except IndexError as e:
     print('You are missing arguments.\nError:', e)
 
 
 # open workbook
-wb = openpyxl.Workbook()
+wb = openpyxl.load_workbook(filename, data_only=True)
 sheet = wb.active
 
 
-# TODO: copy first lines from 1 to row
-
-
-# TODO: add number of blank rows in spreadsheet
-
+# add number of blank rows in spreadsheet
+sheet.insert_rows(starting_row, amount=num_blank_rows)
 
 # save workbook
 wb.save('fileWithBlanks.xlsx')
