4ce476f75fb020fd2b800a78bac5b906ccc9ec1d
Lisa Meyer <lisamermey@gmail.com>
2023-06-05 10:24:56 -0400

    pseudocode

--- ch13/practice/textToSheet.py
+++ ch13/practice/textToSheet.py
@@ -2,6 +2,28 @@
 # textToSheet.py - Reads the contents of several text files and inserts those contents into a spreadsheet.
 
 import openpyxl
+from openpyxl.utils import get_column_letter
+import sys
+
+
+# create new workbook
+
+
+# get files
+
+
+# open file
+
+
+# read each line of file to get a list of strings
+
+
+# get current column letter
+    # if column A1 contains text, move to next column until blank column found.
+
+
+# write each string in list to new row of same column
+
 
 def main():
     return
