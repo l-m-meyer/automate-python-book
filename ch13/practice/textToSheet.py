257ca53cfd9398f466298f4a266ce0f8346007f5
Lisa Meyer <lisamermey@gmail.com>
2023-06-05 10:09:22 -0400

    initial commit

---
+++ ch13/practice/textToSheet.py
@@ -0,0 +1,10 @@
+#! python3
+# textToSheet.py - Reads the contents of several text files and inserts those contents into a spreadsheet.
+
+import openpyxl
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
