063bb411c76194691e065ae11e9b6b6b9f355803
Lisa Meyer <lisamermey@gmail.com>
2023-06-06 12:23:22 -0400

    adds file validation to get_files()

--- ch13/practice/textToSheet.py
+++ ch13/practice/textToSheet.py
@@ -4,6 +4,7 @@
 import openpyxl
 from openpyxl.utils import get_column_letter
 import sys
+import os
 
 
 def create_workbook():
@@ -17,17 +18,40 @@ def create_workbook():
 def get_files():
     # get files from command line
     args = sys.argv[1:]
+    
+    # checks that there is at least one command line argument in args list
+    if not args:
+        args = input("Please include at least one '.txt' file:\n").split(' ')
 
     # filters list for only '.txt' files
     files = list(filter(lambda f: f.endswith('.txt'), args))
-    
+
+    # checks that there is at least one '.txt' file in files list
+    if not files:
+        get_files()
+
+    # checks that files exist
+    files = check_if_exists(files)
+   
     return files
 
 
-# open each file
+def check_if_exists(files):
+    for file in files:
+        if not os.path.exists(file):
+            files.remove(file)
+    return files
 
 
-# read each line of file to get a list of strings
+def process_files(files):
+    for file in files:
+        # open each file
+        with open(file, encoding='utf-8') as f:
+            # read each line of file to get a list of strings
+            lines = f.read().splitlines()
+
+        # print(f'{file} lines:\n', lines)
+
 
 
 # get current column letter
@@ -40,6 +64,8 @@ def get_files():
 def main():
     wb, sheet = create_workbook()
     files = get_files()
+    process_files(files)
+    
     return
 
 if __name__ == '__main__':
