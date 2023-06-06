8b0dfbae25f5965816105725c5f96a09d2778c4f
Lisa Meyer <lisamermey@gmail.com>
2023-06-06 14:44:39 -0400

    final solution

--- ch13/practice/textToSheet.py
+++ ch13/practice/textToSheet.py
@@ -1,10 +1,11 @@
 #! python3
 # textToSheet.py - Reads the contents of several text files and inserts those contents into a spreadsheet.
 
-import openpyxl
-from openpyxl.utils import get_column_letter
-import sys
+
 import os
+import sys
+import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
 
 
 def create_workbook():
@@ -13,6 +14,15 @@ def create_workbook():
     sheet = wb.active
 
     return wb, sheet
+
+
+def check_if_exists(files):
+    for file in files:
+        # removes nonexistent files from files list
+        if not os.path.exists(file):
+            files.remove(file)
+    
+    return files
 
 
 def get_files():
@@ -36,37 +46,40 @@ def get_files():
     return files
 
 
-def check_if_exists(files):
-    for file in files:
-        if not os.path.exists(file):
-            files.remove(file)
-    return files
+def get_current_column(column_letter):
+    # if column_letter has not been defined, set to column A
+    if not column_letter:
+        return get_column_letter(1)
+    
+    # adds 1 to current column index
+    column_index = column_index_from_string(column_letter) + 1
 
+    return get_column_letter(column_index)
 
-def process_files(files):
+    
+def process_files(files, sheet):
+    column_letter = ''
+
     for file in files:
         # open each file
         with open(file, encoding='utf-8') as f:
             # read each line of file to get a list of strings
             lines = f.read().splitlines()
 
-        # print(f'{file} lines:\n', lines)
-
-
-
-# get current column letter
-    # if column A1 contains text, move to next column until blank column found.
-
-
-# write each string in list to new row of same column
+            # get current column letter
+            column_letter = get_current_column(column_letter)
+            
+            # writes each string in list to new row of same column
+            for i, line in enumerate(lines, start=1):
+                sheet[f'{column_letter}{i}'].value = line
 
 
 def main():
     wb, sheet = create_workbook()
     files = get_files()
-    process_files(files)
-    
-    return
+    process_files(files, sheet)
+    wb.save('textToSheet.xlsx')
+
 
 if __name__ == '__main__':
     main()
